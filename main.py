aa2c5aec76135547c4c825839287e943e2832698
m124578n <m23568n@gmail.com>
2023-06-17 17:42:40 +0800

    first

---
+++ main.py
@@ -0,0 +1,36 @@
+from typing import Union, Annotated
+
+from pydantic import BaseModel
+from fastapi import FastAPI, Query
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
+
+
+@app.get("/items/")
+async def read_items(q: Annotated[str | None,
+                                  Query(min_length=3, max_length=50, regex="^fixedquery$")] = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
